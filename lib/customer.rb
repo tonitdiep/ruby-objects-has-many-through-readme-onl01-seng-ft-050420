7c6a2c096a5d8134e004d39314e3f5d263858783
Toni  Diep <adiep123@gmail.com>
2020-05-20 17:39:17 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,12 +1,15 @@
+require 'pry'
 class Customer
   @@all = []
   attr_reader :name, :age
   def initialize (name, age)
     @name = name
     @age = age
-    @@all << self
+    # @@all << self
   end
   def self.all
-    @@all << self
+    # @@all << self
+    @@all = []
   end
+  # binding.pry
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,8 @@
 class Meal
-
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total 
+    @tip = tip
+  end
 end
