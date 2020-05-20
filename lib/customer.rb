775cfc7d5f28669f4f93ceb95503f0b729dee065
Toni  Diep <adiep123@gmail.com>
2020-05-20 17:05:31 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,12 @@
 class Customer
-  
+  @@all = []
+  attr_reader :name, :age
+  def initialize (name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all << self
+  end
 end
